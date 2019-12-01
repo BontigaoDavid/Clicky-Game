c4b71dc89540c301e898be99e8374fa8675f812b
David Bontigao <bontigaodavid@gmail.com>
2019-11-30 21:04:49 -0600

    finished

---
+++ client/build/precache-manifest.ee51cb89a961ccb6e8c2a28f7c3ac476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd17860ad3024880a8fc9c73e2bb8e47",
+    "url": "/Clicky-Game/static/media/9.dd17860a.png"
+  },
+  {
+    "revision": "9d65e3eb8945d087ea887d34b46d7c55",
+    "url": "/Clicky-Game/static/media/7.9d65e3eb.png"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "6426413809c3c2c64f2a",
+    "url": "/Clicky-Game/static/js/main.f268b57b.chunk.js"
+  },
+  {
+    "revision": "7bab197bf41f0b96bdf9",
+    "url": "/Clicky-Game/static/js/2.8155c80b.chunk.js"
+  },
+  {
+    "revision": "6426413809c3c2c64f2a",
+    "url": "/Clicky-Game/static/css/main.5258fa37.chunk.css"
+  },
+  {
+    "revision": "cb61b9d9689f0ed80c52ec896346063e",
+    "url": "/Clicky-Game/index.html"
+  }
+];
